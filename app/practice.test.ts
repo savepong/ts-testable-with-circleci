47d98ef0f6ce5b61114ccb2439f15269a95a68cc
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2020-06-06 14:42:07 +0700

    Done deal

--- app/practice.test.ts
+++ app/practice.test.ts
@@ -1,21 +1,33 @@
-import * as Practice from './practice'
+import { sentence } from './practice'
 
 describe("sentence", () => {
-  it("should be able to handle a single word", () => {
-    expect(Practice.sentence(["Apple"])).toEqual("Apple")
+  it("Can join a word to a single word", () => {
+    const result = sentence(["Chris"])
+
+    expect(result).toEqual("Chris")
   })
 
-  it("should be able to handle two words", () => {
-    expect(Practice.sentence(["Apple", "Orange"])).toEqual("Apple and Orange")
+  it("Can join two words with and", () => {
+    const result = sentence(["Apple", "Orange"])
+
+    expect(result).toEqual("Apple and Orange")
   })
 
-  it("should be able to handle three words", () => {
-    expect(Practice.sentence(["Apple", "Orange", "Banana"])).toEqual("Apple, Orange and Banana")
+  it("Can join three words together", () => {
+    const result = sentence(["Apple", "Orange", "Banana"])
+
+    expect(result).toEqual("Apple, Orange and Banana")
   })
 
-  it("should be able to handle four words", () => {
-    expect(Practice.sentence(["Apple", "Orange", "Banana", "Pineapple"])).toEqual("Apple, Orange, Banana and Pineapple")
+  it("Can join four words together", () => {
+    const result = sentence(["Apple", "Orange", "Banana", "Papaya"])
+
+    expect(result).toEqual("Apple, Orange, Banana and Papaya")
+  })
+
+  it("Can join zero word together", () => {
+    const result = sentence([])
+
+    expect(result).toEqual("")
   })
 })
-
-
--- app/practice.ts
+++ app/practice.ts
@@ -1,9 +1,14 @@
-export function sentence(words: string[]): string {
-  if (words.length <= 2) {
-    return words.join(" and ")
-  }
 
+function joinWords(words: string[], separator: string): string {
+  return words.filter(w => w).join(separator)
+}
+
+export function sentence(words: string[]): string {
   const lastWord = words[words.length - 1]
-  const joinComma = words.slice(0, words.length - 1).join(", ")
-  return `${joinComma} and ${lastWord}`
+  const wordsExceptLastWord = words.slice(0, words.length - 1)
+
+  return joinWords(
+    [wordsExceptLastWord.join(", "), lastWord],
+    " and "
+  )
 }
